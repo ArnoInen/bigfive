3fb8d946b11400d04bce959b785686907fa4894a
arno_inen <aqinen@gmail.com>
2022-03-06 18:50:48 +0100

    Adds a less line //self.skipwaiting

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
-  self.skipWaiting();
+  //self.skipWaiting();
 });
 
 /* Serve cached content when offline */
